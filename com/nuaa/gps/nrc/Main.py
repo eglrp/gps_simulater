efd53093b4e8168620286778ef33341964cfecab
wangjiuyong <wangjiuong@126.com>
2017-12-15 08:07:47 +0800

    代码优化

--- com/nuaa/gps/nrc/Main.py
+++ com/nuaa/gps/nrc/Main.py
@@ -12,35 +12,28 @@ class main_service:
     eph_list_ipil1910 = []
 
     def __init__(self, data_file_directory):
+        self.dic = {'brdc0060.10n': self.eph_list_brdc0060,
+                    'brdc1980.17n': self.eph_list_brdc0060,
+                    'lpil1910.09n': self.eph_list_brdc0060}
         self.parse_directory(data_file_directory)
 
     def parse_directory(self, data_file_directory):
+        [self.aa(data_file_directory, key) for key in self.dic.keys()]
 
-        file_path = data_file_directory + 'brdc0060.10n';
+    def aa(self, data_file_directory, name):
+        file_path = data_file_directory + name;
+        obj = self.dic[name]
         content = self.load_data(file_path)
-        list = self.parse_file(content)
-        self.eph_list_brdc0060 = list
-        print('load data from {} size {}'.format(file_path, len(self.eph_list_brdc0060)))
+        self.parse_file(content, obj)
+        print('load data from {} size {}'.format(file_path, len(obj)))
 
-        file_path = data_file_directory + 'brdc1980.17n';
-        content = self.load_data(file_path)
-        list = self.parse_file(content)
-        self.eph_list_brdc1980 = list
-        print('load data from {} size {}'.format(file_path, len(self.eph_list_brdc1980)))
-
-        file_path = data_file_directory + 'lpil1910.09n';
-        content = self.load_data(file_path)
-        list = self.parse_file(content)
-        self.eph_list_ipil1910 = list
-        print('load data from {} size {}'.format(file_path, len(self.eph_list_ipil1910)))
-
-    def parse_file(self, content):
+    def parse_file(self, content, save_to_list):
         list = []
         for i in range(len(content) // 8):
             sub_content = " ".join(content[i * 8:(i + 1) * 8])
             eph_obj = eph(sub_content)
             list.append(eph_obj)
-        return list
+        save_to_list.extend(list)
 
     def load_data(self, file_path):
         content = readFile(file_path)
